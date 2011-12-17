a29c18cd1d36580f6dacf5589749df39f948901d
rbchan <richard.chandlers@gmail.com>
2011-12-16 22:29:55 -0500

    beginning section on jaguar example

--- Ch9/scratch.R
+++ Ch9/scratch.R
@@ -1,8 +1,10 @@
 
 
+# Homogeneous BPP
 
-set.seed(434)
-N <- 100
+
+set.seed(4234)
+N <- 50
 s <- cbind(runif(N), runif(N)) # points
 # counts in each pixel
 n.k <- table(cut(s[,1], seq(0, 1, 0.2)),
@@ -10,19 +12,110 @@ n.k <- table(cut(s[,1], seq(0, 1, 0.2)),
 
 
 # plot continuous space and discrete space
-x11(width=6, height=3)
+png("figs/homoPlots.png", width=5, height=2.5, units="in", res=400)
 op <- par(mfrow=c(1, 2), mai=c(0.1, 0.1, 0.1, 0.1))
 plot(s, frame=T, ann=FALSE, axes=FALSE, asp=1, cex=0.5)
 segments(seq(0, 1, 0.2), 0, seq(0, 1, 0.2), 1, col=gray(0.5))
 segments(0, seq(0, 1, 0.2), 1, seq(0, 1, 0.2), col=gray(0.5))
-plot(s, frame=T, ann=FALSE, axes=F, type="n")
+plot(s, frame=T, ann=FALSE, axes=F, type="n", asp=1)
 segments(seq(0, 1, 0.2), 0, seq(0, 1, 0.2), 1, col=gray(0.5))
 segments(0, seq(0, 1, 0.2), 1, seq(0, 1, 0.2), col=gray(0.5))
 y <- 0.1
 for(i in 1:nrow(n.k)) {
-    text(seq(0.1, 1, by=0.2), y, labels=n.k[,i])
+    text(seq(0.1, 1, by=0.2), y, labels=n.k[,i], cex=0.6)
     y <- y+0.2
 }
 par(op)
+dev.off()
 
 
+
+
+
+
+
+
+
+# Heterogeneous BPP
+
+
+
+
+
+# spatial covariate
+elev.fn <- function(x) x[,1]+x[,2]
+
+
+# 2-dimensional integration over unit square
+int2d <- function(alpha, delta=0.02) {
+  z <- seq(delta/2, 1-delta/2, delta)
+  len <- length(z)
+  cell.area <- delta*delta
+  S <- cbind(rep(z, each=len), rep(z, times=len))
+  sum(exp(alpha*elev.fn(S)) * cell.area)
+  }
+
+# Simulate PP using rejection sampling
+set.seed(395)
+N <- 100
+count <- 1
+s <- matrix(NA, N, 2)
+alpha <- 2 # parameter of interest
+while(count <= 100) {
+  x.c <- runif(1, 0, 1)
+  y.c <- runif(1, 0, 1)
+  s.cand <- cbind(x.c,y.c)
+  elev.min <- elev.fn(cbind(-1,-1))
+  elev.max <- elev.fn(cbind(1,1))
+  pr <- exp(alpha*elev.fn(s.cand)) / int2d(alpha)
+  Q <- max(c(exp(alpha*elev.min) / int2d(alpha),
+             exp(alpha*elev.max) / int2d(alpha)))
+  if(runif(1) < pr/Q) {
+    s[count,] <- s.cand
+    count <- count+1
+    cat("accept\n")
+    }
+  else
+    cat("  reject\n")
+  }
+
+
+
+
+
+
+n.k <- table(cut(s[,1], seq(0, 1, 0.2)),
+             cut(s[,2], seq(0, 1, 0.2)))
+
+
+# plot continuous space and discrete space
+png("figs/heteroPlots.png", width=5, height=2.5, units="in", res=400)
+op <- par(mfrow=c(1, 2), mai=c(0.1, 0.1, 0.1, 0.1))
+plot(s, frame=T, ann=FALSE, axes=FALSE, asp=1, cex=0.5)
+segments(seq(0, 1, 0.2), 0, seq(0, 1, 0.2), 1, col=gray(0.5))
+segments(0, seq(0, 1, 0.2), 1, seq(0, 1, 0.2), col=gray(0.5))
+plot(s, frame=T, ann=FALSE, axes=F, type="n", asp=1)
+segments(seq(0, 1, 0.2), 0, seq(0, 1, 0.2), 1, col=gray(0.5))
+segments(0, seq(0, 1, 0.2), 1, seq(0, 1, 0.2), col=gray(0.5))
+y <- 0.1
+for(i in 1:nrow(n.k)) {
+    text(seq(0.1, 1, by=0.2), y, labels=n.k[,i], cex=0.6)
+    y <- y+0.2
+}
+par(op)
+dev.off()
+
+
+
+
+# plot the simulated data
+png("../figs/elevMap.png", width=7, height=7, units="in", res=400)
+Sx <- seq(-1, 1, length=50)
+len <- length(Sx)
+S <- cbind(rep(Sx, each=len), rep(Sx, times=len))
+elev <- elev.fn(S)
+image(Sx, Sx, matrix(elev, len), col=rgb(0,seq(0.1,1,0.01),0,0.8),
+      xlab="Easting", ylab="Northing", main="Elevation map")
+box()
+points(s, pch=16)
+dev.off()
