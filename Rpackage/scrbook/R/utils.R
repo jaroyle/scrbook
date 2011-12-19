f613499d7ebf354bae6f02c5ebf8d6ca6841f1a1
andy royle <jaroyle@gmail.com>
2011-12-19 09:55:15 -0500

    added a couple utility functions

--- Rpackage/scrbook/R/utils.R
+++ Rpackage/scrbook/R/utils.R
@@ -16,3 +16,59 @@ e2dist <- function (x, y)
 
 
 
+image.scale <-
+function (z, col, x, y = NULL, size = NULL, digits = 2, labels = c("breaks", 
+    "ranges"))
+{
+    # sort out the location
+    n <- length(col)
+    usr <- par("usr")
+    mx <- mean(usr[1:2]); my <- mean(usr[3:4])
+    dx <- diff(usr[1:2]); dy <- diff(usr[3:4])
+    if (missing(x))
+        x <- mx + 1.05*dx/2	# default x to right of image
+    else if (is.list(x)) {
+        if (length(x$x) == 2) 
+          size <- c(diff(x$x), -diff(x$y)/n)
+        y <- x$y[1]
+        x <- x$x[1]
+    } else x <- x[1]
+    if (is.null(size))
+        if (is.null(y)) {
+          size <- 0.618*dy/n	# default size, golden ratio
+          y <- my + 0.618*dy/2	# default y to give centred scale
+        } else size <- (y-my)*2/n
+    if (length(size)==1)
+        size <- rep(size, 2)	# default square boxes
+    if (is.null(y))
+        y <- my + n*size[2]/2
+    # draw the image scale
+    i <- seq(along = col)
+    rect(x, y - i * size[2], x + size[1], y - (i - 1) * size[2], 
+        col = rev(col), xpd = TRUE)
+    # sort out the labels
+    rng <- range(z, na.rm = TRUE)
+    bks <- seq(from = rng[2], to = rng[1], length = n + 1)
+    bks <- formatC(bks, format="f", digits=digits)
+    labels <- match.arg(labels)
+    if (labels == "breaks")
+        ypts <- y - c(0, i) * size[2]
+    else {
+        bks <- paste(bks[-1], bks[-(n+1)], sep = " - ")
+        ypts <- y - (i - 0.5) * size[2]
+    }
+    text(x = x + 1.2 * size[1], y = ypts, labels = bks, adj =
+        ifelse(size[1]>0, 0, 1), xpd = TRUE) 
+}
+
+
+
+spatial.plot <-
+function(x,y,add=TRUE,cx=1){
+ nc<-as.numeric(cut(y,20))
+if(!add) plot(x,pch=" ")
+ points(x,pch=20,col=terrain.colors(20)[nc],cex=cx)
+image.scale(y,col=terrain.colors(20))
+
+}
+
