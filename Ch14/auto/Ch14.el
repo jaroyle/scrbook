9aa5ce6ae15be4befdb17f3d03907c3bfbc55682
Richard Chandler <richard.chandlers@gmail.com>
2012-01-04 13:44:58 -0500

    added new directory for Ch14

---
+++ Ch14/auto/Ch14.el
@@ -0,0 +1,5 @@
+(TeX-add-style-hook "Ch14"
+ (lambda ()
+    (LaTeX-add-labels
+     "chapt.scr-unmarked")))
+
---
+++ Ch14/buildPreview.bat
@@ -0,0 +1,4 @@
+bibtex previewCh14.tex
+bibtex previewCh14.tex
+pdflatex previewCh14.tex
+open previewCh14.pdf
