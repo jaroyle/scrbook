5ae37d542430673d6905ea1b5cfa1c3d3b65c515
Richard Chandler <richard.chandlers@gmail.com>
2012-01-05 12:52:35 -0500

    added batch files to build chapters and clean up

--- Ch6/buildPreview.bat
+++ Ch6/buildPreview.bat
@@ -1,5 +1,5 @@
-pdflatex previewCh6.tex
-bibtex previewCh6
-bibtex previewCh6
-pdflatex previewCh6.tex
+pdflatex previewCh6 -include-directory=../ -output-directory=../
+bibtex ../previewCh6 -include-directory=../
+pdflatex previewCh6.tex -include-directory=../
 open previewCh6.pdf
+rm ../previewCh6.aux ../previewCh6.bbl ../previewCh6.blg ../previewCh6.log ../previewCh6.out ../previewCh6.pdf
