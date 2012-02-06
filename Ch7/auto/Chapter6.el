103d74f05582b9af038c3e15ede842dc5b49c532
Andy Royle <jaroyle@gmail.com>
2012-02-06 16:48:26 -0500

    updating 7

---
+++ Ch7/auto/Chapter6.el
@@ -0,0 +1,15 @@
+(TeX-add-style-hook "Chapter6"
+ (lambda ()
+    (LaTeX-add-labels
+     "chapt.closed"
+     "densityvsdetection.fig"
+     "mcmc.eq.bayes"
+     "mcmc.eq.mu-posterior"
+     "eq. 3"
+     "postdist.fig"
+     "plotsofPD.fig"
+     "slicesample.fig"
+     "timeseries.fig"
+     "timeseries2.fig"
+     "pantercamera.fig")))
+
