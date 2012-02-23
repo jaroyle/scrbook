80e7dd83e89276c82fe4ca7374719f520aa9ab02
Richard Chandler <richard.chandlers@gmail.com>
2012-02-23 14:10:28 -0500

    working on another figure to show the distortion of space

---
+++ Ch10/R/distort.R
@@ -0,0 +1,52 @@
+
+# Make a cool figure
+
+library(gdistance)
+
+len <- 50
+sp <- seq(0,1,len=len)
+u <- cbind(rep(sp,len), rep(sp,each=len))
+
+
+fu <- function(x, b=-10) {
+    exp(0.5 + b*x[1] + -b*x[1]^2)
+}
+elev <- apply(u, 1, fu)
+elev <- matrix(elev, len, len)
+plot(raster(elev))
+range(elev)
+
+p <- apply(u, 1, function(x) {
+    d <- sqrt((x[1]-0.5)^2 + (x[2]-0.5)^2)
+    p <- 0.5*exp(-d^2/(2*0.05))
+    p
+})
+p <- matrix(p, len)
+image(sp, sp, p)
+
+
+elev.z <- elev-min(elev)
+elev.z <- elev.z/max(elev.z)
+cost <- exp(3*raster(matrix(elev.z, len)))
+tr <- transition(cost, function(x) mean(1/x), directions=16)
+tr1 <- geoCorrection(tr)
+plot(raster(tr1))
+
+lcd <- costDistance(tr1, c(0.5,0.5), u)
+lcdr <- raster(matrix(lcd, len, len))
+plot(lcdr)
+
+p2 <- 0.5*exp(-lcd^2/(2*0.1))
+p2 <- matrix(p2, len)
+image(sp, sp, p2)
+
+
+png("../figs/distort.png", width=6, height=1.8, units="in", res=400)
+op <- par(mfrow=c(1,3), mai=c(0.1,0.1,0.1,0.1))
+pr <- seq(0, 1, by=0.1)
+contour(sp, sp, t(p), levels=pr, axes=FALSE, frame=TRUE)
+plot(raster(elev.z), axes=FALSE)
+contour(sp, sp, p2, levels=pr, axes=FALSE, frame=TRUE)
+par(op)
+dev.off()
+
