72c6b5547d8aa576eb84cf05842aace51f0c8063
Richard Chandler <richard.chandlers@gmail.com>
2012-03-08 15:43:06 -0500

    new idea for SCRDS chapter

---
+++ Ch17/R/simdata.R
@@ -0,0 +1,59 @@
+
+# Simulate quadrat count data
+# guys have activity centers and they move
+
+library(raster)
+
+
+# Dimensions of the state-space
+xlims <- ylims <- c(0, 100)
+
+# As a raster
+r <- raster(xmn=xlims[1], xmx=xlims[2], ymn=ylims[1], ymx=ylims[2])
+res(r) <- c(5,5)
+values(r) <- 1
+r
+plot(r)
+
+
+# Traps
+buff <- 25
+xc <- seq(xlims[1]+buff, xlims[2]-buff, by=5)
+lx <- length(xc)
+X <- cbind(rep(xc, times=lx), rep(xc, each=lx))
+dim(X)
+
+
+# guys have activity centers (s) and coordinates at time k (u)
+N <- 50
+K <- 5
+tau <- 2
+s <- cbind(runif(N, xlims[1], ylims[2]), runif(N, ylims[1], ylims[2]))
+u <- array(NA, c(N, 2, K))
+#plot(r)
+plot(X, pch="+", xlim=xlims, ylim=ylims, asp=1)
+points(s, pch=16, col=4)
+for(k in 1:K) {
+    u[,,k] <- cbind(rnorm(N, s[,1], tau), rnorm(N, s[,2], tau))
+    points(u[,1,k], u[,2,k], pch=16, col=3, cex=0.5)
+}
+
+# tabulate the number of guys in each trap pixel (quadrat)
+# True abundance at trap pixels during each occasion
+Nj <- matrix(0, nrow(X), K)
+rownames(Nj) <- cellFromXY(r, X)
+for(k in 1:K) {
+    cells <- cellFromXY(r, u[,,k])
+    counts <- table(cells)
+    counts.in <- counts[names(counts) %in% rownames(n)]
+    Nj[names(counts.in),k] <- counts.in
+}
+
+Nj
+
+
+# Imperfect detection
+p <- 0.5
+n <- Nj
+n[] <- rbinom(prod(dim(Nj)), Nj, p)
+n
