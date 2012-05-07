e654c04c264f0f78fdcc5fe74744c63ca76d068e
andy royle <jaroyle@gmail.com>
2012-05-07 14:53:29 -0400

    a few hacks + new pics for ch1

--- Rpackage/scrbook/R/hra.R
+++ Rpackage/scrbook/R/hra.R
@@ -1,12 +1,12 @@
 hra <-
-function(func,parms,plot=TRUE,xlim,ylim){
+function(func,parms,plot=TRUE,xlim,ylim,ng=100,target.area=NULL){
 
 # individual centered at (3,3) with 10000 "potential trap" locations
 s<-c( (xlim[2]-xlim[1])/2, (ylim[2]-ylim[1])/2 )
-x1<- rep(seq(xlim[1],xlim[2],,100),100)
-x2<- sort(rep(seq(ylim[1],ylim[2],,100),100))
+x1<- rep(seq(xlim[1],xlim[2],,ng),ng)
+x2<- sort(rep(seq(ylim[1],ylim[2],,ng),ng))
 delta<- min(diff(x1[1:10]))
-#area<- ( (6+delta/2)^2)/10000
+#area<- ( (6+delta/2)^2)/(ng*ng)
 # plot those traps
 X<-cbind(x1,x2)
 #plot(X,pch=".")
@@ -37,12 +37,13 @@ psi<- p/sum(p)
 # symmetric integration problem.
 # sum cells < k, and then vary k
 
+if(is.null(target.area)){
 # find area that contains 95% of movements
 x0<-.2
 repeat{
 in.hr<- D<=x0
 total<- sum(psi[in.hr])  
-cat("Total probability: ",total,fill=TRUE)
+#cat("Total probability: ",total,fill=TRUE)
 if(total>=.95) {
 print(x0)    # if condition is met, break
 break
@@ -55,4 +56,41 @@ cat("radius to achieve 95% of area: ", radius, fill=TRUE)
 area<- pi*radius^2
 cat("home range area: ", area,fill=TRUE)
 return(area)
+}
+
+if(!is.null(target.area)){
+
+if(is.null(target.area)){ 
+cat("need target.area",fill=TRUE)
+goose.egg<-NULL
+return(goose.egg)
+}
+
+obj<-function(beta2){
+p<- func(c(parms[1],beta2),D)
+psi<- p/sum(p)
+x0<-.1
+repeat{
+in.hr<- D<=x0
+total<- sum(psi[in.hr])  
+#cat("Total area: ",total,fill=TRUE)
+if(total>=.95) {
+#print(x0)
+break
+}
+x0<-x0*1.01
+}
+hr.area<- pi*x0*x0
+#print(hr.area)
+ss<-  (hr.area - target.area)^2
+ss
+}
+
+tmp<-optimize(obj,interval=c(.01,5))
+beta2<-tmp$minimum
+cat("Value of parm[2] to achieve 95% home range area of ", target.area," : ",beta2,fill=TRUE)
+return(beta2)
+}
+
+
 }
