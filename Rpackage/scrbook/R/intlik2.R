fe1b5a98cb48a60ac4969e46178ad3502b97baa1
andy royle <jaroyle@gmail.com>
2012-10-02 10:05:42 -0400

    few edits MLE

--- Rpackage/scrbook/R/intlik2.R
+++ Rpackage/scrbook/R/intlik2.R
@@ -7,17 +7,17 @@ Yu<-max(X[,2])+ ssbuffer
 Yl<-min(X[,2])- ssbuffer
 
 #delta<- (Xu-Xl)/npix
-xg<-seq(Xl+delta/2,Xu-delta/2,delta) 
-yg<-seq(Yl+delta/2,Yu-delta/2,delta) 
+xg<-seq(Xl+delta/2,Xu-delta/2,delta)
+yg<-seq(Yl+delta/2,Yu-delta/2,delta)
 npix.x<-length(xg)
 npix.y<-length(yg)
 area<- (Xu-Xl)*(Yu-Yl)/((npix.x)*(npix.y))
 G<-cbind(rep(xg,npix.y),sort(rep(yg,npix.x)))
 nG<-nrow(G)
-D<- e2dist(X,G)  
+D<- e2dist(X,G)
 
 alpha0<-parm[1]
-alpha1<-parm[2]
+alpha1<-exp(parm[2])
 n0<-exp(parm[3])
 probcap<- plogis(alpha0)*exp(-alpha1*D*D)
 Pm<-matrix(NA,nrow=nrow(probcap),ncol=ncol(probcap))
@@ -27,8 +27,8 @@ lik.marg<-rep(NA,nrow(ymat))
 for(i in 1:nrow(ymat)){
 Pm[1:length(Pm)]<- (dbinom(rep(ymat[i,],nG),K,probcap[1:length(Pm)],log=TRUE))
 lik.cond<- exp(colSums(Pm))
-lik.marg[i]<- sum( lik.cond*(1/nG) )  
-}                                                 
+lik.marg[i]<- sum( lik.cond*(1/nG) )
+}
 nv<-c(rep(1,length(lik.marg)-1),n0)
 part1<- lgamma(nrow(y)+n0+1) - lgamma(n0+1)
 part2<- sum(nv*log(lik.marg))
