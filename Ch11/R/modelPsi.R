d32059976466f79f27a448be21ce5f0a2ed32686
Richard Chandler <richard.chandlers@gmail.com>
2012-10-09 10:02:42 -0400

    epic epiphany or I was an idiot

--- Ch11/R/modelPsi.R
+++ Ch11/R/modelPsi.R
@@ -1,4 +1,9 @@
 
+# There are two ways to fit inhomogeneous point process SCR model
+# (1) Change the prior from uniform to weighted
+# (2) Thin uniform point process by modeling psi
+
+# This script does method (2)
 
 
 
@@ -7,37 +12,36 @@
 
 
 
-
-
-
+# Trap locations in [0,1] x [0,1] state-space
 X <- cbind(rep(seq(.3,.7,,5), 5), rep(seq(.3,.7,,5), times=5))
 X
 
-x <- seq(-2, 2, .01)
-plogis(-2 + 2*.1)/sum(plogis(-2 + 2*x))
 
 
-M <- 200
+# Spatial covariate. Elevation as function of location
 elev <- function(x) x[1]+x[2]
 grid <- expand.grid(seq(0,1,.1),seq(0,1,0.1))
 library(lattice)
 levelplot(apply(grid, 1, elev) ~ grid[,1] + grid[,2])
 
-sM <- cbind(runif(M), runif(M))
-pr <- plogis(-2 + 2*apply(sM, 1, elev))
-w <- rbinom(M, 1, pr)
 
-plot(sM)
-points(sM[w==1,], fg="blue", pch=16)
+# Simulate data
+M <- 200 # Data augmentation size. Could be anything, but we need it
+s <- cbind(runif(M), runif(M)) # activity centers
+pr <- plogis(-2 + 2*apply(s, 1, elev))  # thinning prob (data aug prob)
+w <- rbinom(M, 1, pr) # Thinned guys
 
-p0 <- 0.7
-sigma <- 0.2
-J <- nrow(X)
-K <- 5
+plot(s)
+points(s[w==1,], col=rgb(0,0,1,0.5), pch=16)
+
+p0 <- 0.7       # Basal capture prob
+sigma <- 0.1    # Half-normal scale parameters
+J <- nrow(X)    # nTraps
+K <- 5          # nOcc
 y <- matrix(NA, M, J)
 for(i in 1:M) {
     for(j in 1:J) {
-        d2 <- (sM[i,1]-X[j,1])^2 + (sM[i,2]-X[j,2])^2
+        d2 <- (s[i,1]-X[j,1])^2 + (s[i,2]-X[j,2])^2
         p <- w[i]*p0*exp(-d2/(2*sigma^2))
         y[i,j] <- rbinom(1, K, p)
     }
@@ -57,6 +61,7 @@ library(rjags)
 dat1 <- list(y=y, X=X, J=J, K=K, M=M)
 par1 <- c("beta0", "beta1", "sigma", "p0", "N")
 init1 <- function() list(w=rep(1, dat1$M))
+
 jm1 <- jags.model("modelPsi.txt", data=dat1, init1,
                   n.chains=2, n.adapt=500)
 jc1 <- coda.samples(jm1, par1, n.iter=1500)
