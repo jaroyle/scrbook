4299509b7c52c0153db7ec24019b460aadae2e45
andy royle <jaroyle@gmail.com>
2012-11-18 13:52:06 -0500

    few edts RSF ms

--- Ch4-SCR0/figs/det_functions.R
+++ Ch4-SCR0/figs/det_functions.R
@@ -1,17 +1,14 @@
-figname<-"blahblah"
+figname<-"det_functions.png"
 
 ### not sure of dimensions to match the current version of the fig.
-png(figname,width=7,height=7, units="in", res=400)
+png(figname,width=7,height=4.5, units="in", res=400)
 
 x<- seq(.011,20,,100)
-
-want to choose functions so that 
-p0 = some number
-p(dist 5) = some number.
+.
 
 k<- exp(-(1/(2*6*6))*x^2)
 p1<- .65*k
-plot(x,p1,type="l",lwd=2)
+plot(x,p1,type="l",lwd=2,xlab="distance",ylab="encounter probability")
 p2<- 1-exp(-p1)
 lines(x,p2,lty=2,lwd=2)
 
@@ -25,5 +22,6 @@ lp4<-  0 - (1/(2*6*6))*x*x
 p4<-exp(lp4)/(1+exp(lp4))
 lines(x,p4,lty=4,lwd=2)
 
+legend(12,.6,legend=c("Gaussian detection","Gaussian hazard","negative exponential","logistic"),lty=1:4,cex=.8)
 
 dev.off()
