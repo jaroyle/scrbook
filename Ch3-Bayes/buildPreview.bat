423ad5b53c9c664aa25324b8430b384050dbfc9b
Kimberly Gazenski <kgazenski@usgs.gov>
2013-01-25 13:11:29 -0500

    merging files

--- Ch3-Bayes/buildPreview.bat
+++
@@ -1,5 +0,0 @@
-pdflatex previewCh2 -include-directory=../ -output-directory=../
-bibtex ../previewCh2 -include-directory=../
-pdflatex previewCh2.tex -include-directory=../
-open previewCh2.pdf
-rm ../previewCh2.aux ../previewCh2.bbl ../previewCh2.blg ../previewCh2.log ../previewCh2.out ../previewCh2.pdf
