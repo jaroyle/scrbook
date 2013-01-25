89301b4d2c857edd6ef34f6b20888562e0b14751
Kimberly Gazenski <kgazenski@usgs.gov>
2013-01-25 13:07:53 -0500

    text deleted from ch 18

--- Ch18-Unmarked/buildPreview.bat
+++
@@ -1,5 +0,0 @@
-pdflatex previewCh14 -include-directory=../ -output-directory=../
-bibtex ../previewCh14 -include-directory=../
-pdflatex previewCh14.tex -include-directory=../
-open previewCh14.pdf
-rm ../previewCh14.aux ../previewCh14.bbl ../previewCh14.blg ../previewCh14.log ../previewCh14.out ../previewCh14.pdf
