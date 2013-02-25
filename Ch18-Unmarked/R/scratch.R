f5eea7614ab14d6b450dc76e8e9bf74c853e7383
Richard Chandler <richard.chandlers@gmail.com>
2013-02-25 11:58:09 -0500

    unmarked

--- Ch18-Unmarked/R/scratch.R
+++ Ch18-Unmarked/R/scratch.R
@@ -24,14 +24,24 @@ library(scrbook)
 dist <- e2dist(rbind(s1,s2), x)
 lam0 <- 10
 lambda <- lam0*exp(-dist^2/(2*sigma^2))
-y <- rpois(nrow(x), colSums(lambda))
+n <- rpois(nrow(x), colSums(lambda))
 
 
 
 # Figures
 
+pdf("../figs/heuristic.pdf", width=6, height=6)
+par(mai=rep(0.1,4))
+plot(0, type="n", xlim=c(-xl-1, xl+1), asp=1, ann=FALSE,
+     axes=FALSE, frame=TRUE)
+points(rbind(s1, s2), pch=16, cex=1.2)
+points(x, pch="+", cex=0.9, col=gray(0.5))
+text(x, labels=ifelse(y>0, y, ""), cex=2)
+dev.off()
+system("open ../figs/heuristic.pdf")
 
-png("../figs/heuristic.png", width=6, height=3, res=400, units="in")
+
+png("../figs/heuristic2.png", width=6, height=3, res=400, units="in")
 par(mfrow=c(1,2), mai=rep(0.1,4))
 plot(0, type="n", xlim=c(-xl-1, xl+1), asp=1, ann=FALSE,
 #     xlab="Easting", ylab="Northing",
