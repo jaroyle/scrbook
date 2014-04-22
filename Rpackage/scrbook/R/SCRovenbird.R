a723a76e2ee5324b06040495e9e1e1ffd2371114
andy royle <jaroyle@gmail.com>
2014-04-22 13:09:37 -0400

    ovenbrid scripts

--- Rpackage/scrbook/R/SCRovenbird.R
+++ Rpackage/scrbook/R/SCRovenbird.R
@@ -5,6 +5,7 @@ library("secr")
 data(ovenbird)
 set.seed(2013)
 
+
 png("Ovenbird_traps.png",width=7,height=7, units="in", res=400)
 par(mfrow=c(1,3))
  plot(ovenCH[["2005"]])
@@ -20,38 +21,48 @@ ntraps<- nrow(traps[[1]])
 
 ## Y are the encounter history data
 Y<-ovenCH
-K<-10  # number of samples in each year
+K<-10  # number of sample occasions in each year
 M<-100 # do constant data augmentation to all years
 
 ## starting values for each individual's activity centers
 Sst0<-cbind(runif(M,xlim[1],xlim[2]),runif(M,ylim[1],ylim[2]))
 Sst<-NULL
-Ymat<-NULL
+Ycat<-NULL
 died<-NULL
-# make the data into a 3-d array
 
+####
+####  This look stacks all of the data matrices into one large data matrix.
+####
 for(i in 1:5){
-tmp<-Y[[i]]
-nind<-nrow(tmp)
-nrep<-ncol(tmp)
-D<-matrix(0,nrow=M,ncol=10)
-D[1:nind,1:nrep]<-tmp
-D[D>0]<- 0
-D[D<0]<- 1
-died<-rbind(died,D)
+   tmp<-Y[[i]]
+   nind<-nrow(tmp)
+   nrep<-ncol(tmp)
+ ## make an indicator variable of "died"
+   D<-matrix(0,nrow=M,ncol=10)
+   D[1:nind,1:nrep]<-tmp
+   D[D>0]<- 0
+   D[D<0]<- 1
+   died<-rbind(died,D)
 
-tmp[tmp<0]<- 0 ## dead guy set to 0 b/c "died" created above
-tmp[tmp==0]<-ntraps+1
-tmp2<-matrix(NA,nrow=M,ncol=10)  # pad last col with NA for year 1
-tmp2[,1:nrep]<-ntraps+1
-tmp2[1:nind,1:nrep]<-tmp
-Ymat<- rbind(Ymat, tmp2)
+  tmp[tmp<0]<- 0 ## dead guy set to 0 b/c "died" created above
+  tmp[tmp==0]<-ntraps+1
 
-sout<-spiderplot(tmp2[1:nind,1:nrep],as.matrix(X[[i]]))$avg.s
-Stmp<-Sst0
-Stmp[1:nind,1:2]<-sout
-Sst<-rbind(Sst,Stmp)
+  # In year 1 there were 9 sample occasions so we pad the 10th with NA for that year
+  # We indicate non-capture by "J+1" as the convention for specification
+  # of the categorical distribution in BUGS
+  tmp2<-matrix(NA,nrow=M,ncol=10)
+  tmp2[,1:nrep]<-ntraps+1
+  tmp2[1:nind,1:nrep]<-tmp
+
+  Ycat<- rbind(Ycat, tmp2)
+
+  sout<-spiderplot(tmp2[1:nind,1:nrep],as.matrix(X[[i]]))$avg.s
+  Stmp<-Sst0
+  Stmp[1:nind,1:2]<-sout
+  Sst<-rbind(Sst,Stmp)
 }
+ ### This loop will determine whether an individual is known to be dead so that it's encounter probability
+ ### is zeroed out
 for(i in 1:nrow(died)){
 xx<-died[i,]
 if(sum(xx)>0){
@@ -61,12 +72,12 @@ died[i,first]<-0
 }
 }
 
-## 
+##
 ## This bit of code dumps out the BUGS model file
 ##
 cat("
 model {
- # year-specific N parameterized in DA parameter 
+ # year-specific N parameterized in DA parameter
 
 alpha0 ~ dnorm(0,.1)
 sigma ~dunif(0,200)
@@ -92,14 +103,16 @@ for(k in 1:K){
  Ycat[i,k] ~ dcat(cp[i,k,])
 
   for(j in 1:ntraps){
-    lp[i,k,j] <- exp(alpha0 - alpha1*d2[i,j])*z[i]*(1-died[i,k])        
+    ## This is where the died[,] indicator is used to zero-out encounter probability for
+    ## dead individuals
+    lp[i,k,j] <- exp(alpha0 - alpha1*d2[i,j])*z[i]*(1-died[i,k])
     cp[i,k,j] <- lp[i,k,j]/(1+sum(lp[i,k,1:ntraps]))
   }
   cp[i,k,ntraps+1] <- 1-sum(cp[i,k,1:ntraps])  # last cell = not captured
 
 }
 }
-   
+
 }
 ",file="model.txt")
 ###
@@ -118,18 +131,18 @@ for(i in 1:5){
 zst<-c(zst,rep(1,nind[i]),rep(0,M-nind[i]))
 }
 
-inits <- function(){list (z=zst,sigma=runif(1,50,100) ,S=Sst,alpha0=runif(1,-2,-1) 
-,alpha2=-2,alpha3=-2) }              
+inits <- function(){list (z=zst,sigma=runif(1,50,100) ,S=Sst,alpha0=runif(1,-2,-1)
+,alpha2=-2,alpha3=-2) }
 ## parameters to monitor
 parameters <- c("psi","alpha0","alpha1","sigma","N","D")
-## data used in BUGS model                                                                  
-data <- list (died=died,yrdummy=yrdummy,year=yrid,X=as.matrix(X[[1]]),K=10,Ycat=Ymat,bigM=bigM,ntraps=ntraps,ylim=ylim,xlim=xlim)         
+## data used in BUGS model
+data <- list (died=died,yrdummy=yrdummy,year=yrid,X=as.matrix(X[[1]]),K=10,Ycat=Ycat,bigM=bigM,ntraps=ntraps,ylim=ylim,xlim=xlim)
 
 ##
 ### This takes ~1 hour or so to run
 ##
 library("R2jags")
-out <- jags(data, inits, parameters, "model.txt", n.thin=1,n.chains=3, 
+out <- jags(data, inits, parameters, "model.txt", n.thin=1,n.chains=3,
 n.burnin=1000,n.iter=2000,DIC=FALSE)
 
 out
